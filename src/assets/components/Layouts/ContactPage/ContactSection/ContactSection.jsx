b20c75160a4b0b99f1577a107a73e6037f62a2c1
bandarsabu <akhiratul2002@gmail.com>
2024-06-04 05:06:45 +0800

    update responsive contac & donation

--- src/assets/components/Layouts/ContactPage/ContactSection/ContactSection.jsx
+++ src/assets/components/Layouts/ContactPage/ContactSection/ContactSection.jsx
@@ -1,38 +1,38 @@
 const ContactSection = () => {
   return (
-    <section className="w-[1107px] flex gap-[61px] mt-20">
-      <div className="w-[322px] h-[241px] rounded-[20px] shadow-card flex flex-col items-center">
-        <div className="w-[189px] h-[110px] rounded-b-[20px] bg-[#FA9F42] flex items-center justify-center">
+    <section className="w-full flex flex-col lg:flex-row justify-center items-center gap-4 lg:gap-8 mt-20 px-4 ">
+      <div className="w-full lg:w-[322px] h-[241px] rounded-[20px] shadow-card flex flex-col items-center">
+        <div className="w-full lg:w-[189px] h-[110px] rounded-b-[20px] bg-[#FA9F42] flex items-center justify-center">
           <img
             src="/images/ContactPage/contact-telp.png"
             alt=""
             className="w-[53px] h-[46px]"
           />
         </div>
-        <h1 className="text-[36px] font-bold mt-3">Hotline</h1>
-        <h1 className="text-[24px]">0800-987-6543</h1>
+        <h1 className="text-[24px] lg:text-[36px] font-bold mt-3">Hotline</h1>
+        <h1 className="text-[18px] lg:text-[24px]">0800-987-6543</h1>
       </div>
-      <div className="w-[322px] h-[241px] rounded-[20px] shadow-card flex flex-col items-center">
-        <div className="w-[189px] h-[110px] rounded-b-[20px] bg-[#FA9F42] flex items-center justify-center">
+      <div className="w-full lg:w-[322px] h-[241px] rounded-[20px] shadow-card flex flex-col items-center">
+        <div className="w-full lg:w-[189px] h-[110px] rounded-b-[20px] bg-[#FA9F42] flex items-center justify-center">
           <img
             src="/images/ContactPage/contact-msg.png"
             alt=""
             className="w-[50px] h-[36px]"
           />
         </div>
-        <h1 className="text-[36px] font-bold mt-3">Email</h1>
-        <h1 className="text-[24px]">heywani@gmail.com</h1>
+        <h1 className="text-[24px] lg:text-[36px] font-bold mt-3">Email</h1>
+        <h1 className="text-[18px] lg:text-[24px]">heywani@gmail.com</h1>
       </div>
-      <div className="w-[322px] h-[241px] rounded-[20px] shadow-card flex flex-col items-center">
-        <div className="w-[189px] h-[110px] rounded-b-[20px] bg-[#FA9F42] flex items-center justify-center">
+      <div className="w-full lg:w-[322px] h-[241px] rounded-[20px] shadow-card flex flex-col items-center">
+        <div className="w-full lg:w-[189px] h-[110px] rounded-b-[20px] bg-[#FA9F42] flex items-center justify-center">
           <img
             src="/images/ContactPage/contact-location.png"
             alt=""
             className="w-[64px] h-[55px]"
           />
         </div>
-        <h1 className="text-[36px] font-bold mt-3">Alamat</h1>
-        <h1 className="text-[24px]">Pulau Jawa</h1>
+        <h1 className="text-[24px] lg:text-[36px] font-bold mt-3">Alamat</h1>
+        <h1 className="text-[18px] lg:text-[24px]">Pulau Jawa</h1>
       </div>
     </section>
   );
--- src/assets/components/Layouts/ContactPage/FormSection/FormSection.jsx
+++ src/assets/components/Layouts/ContactPage/FormSection/FormSection.jsx
@@ -2,42 +2,44 @@ const FormSection = () => {
   return (
     <section className="mt-[49px] flex flex-col gap-[49px] mb-[31px]">
       <h1 className="text-[36px] font-bold">
-        Kami Siap Membantu Anda Kirimkan Pesan Kepada Kami!{' '}
+        Kami Siap Membantu Anda Kirimkan Pesan Kepada Kami!{" "}
       </h1>
-      <div className="w-[1113px] h-[670px] rounded-[20px] bg-[#FA9F42] py-[26px] pl-[61px] pr-[47px] flex flex-col gap-5">
+      <div className="w-full md:w-[1113px] h-[auto] md:h-[670px] rounded-[20px] bg-[#FA9F42] py-[26px] pl-[61px] pr-[47px] flex flex-col gap-5">
         <div className="flex flex-col gap-[10px]">
           <h1 className="font-semibold text-[30px]">Nama</h1>
           <input
             type="text"
-            className="w-[1005px] h-[66px] rounded-[20px] py-3 px-6 text-2xl font-medium"
+            className="w-full h-[66px] rounded-[20px] py-3 px-6 text-2xl font-medium"
           />
         </div>
-        <div className="w-[1005px] flex gap-[47px]">
+        <div className="w-full md:flex gap-[47px]">
           <div className="flex flex-col gap-[10px]">
             <h1 className="font-semibold text-[30px]">No. Handphone</h1>
             <input
               type="text"
-              className="w-[483px] h-[66px] rounded-[20px] py-3 px-6 text-2xl font-medium"
+              className="w-full md:w-[483px] h-[66px] rounded-[20px] py-3 px-6 text-2xl font-medium"
             />
           </div>
           <div className="flex flex-col gap-[10px]">
             <h1 className="font-semibold text-[30px]">Email</h1>
             <input
               type="text"
-              className="w-[483px] h-[66px] rounded-[20px] py-3 px-6 text-2xl font-medium"
+              className="w-full md:w-[483px] h-[66px] rounded-[20px] py-3 px-6 text-2xl font-medium"
             />
           </div>
         </div>
-        <div className="w-[1005px] flex gap-[47px]">
+        <div className="w-full md:flex gap-[47px]">
           <div className="flex flex-col gap-[10px]">
             <h1 className="font-semibold text-[30px]">Pesan</h1>
             <textarea
               type=""
-              className="w-[1005px] h-[221px] rounded-[20px] pt-6 px-6 text-2xl font-medium"
+              cols={80} // Menentukan jumlah kolom
+              className="w-full h-[221px] rounded-[20px] pt-6 px-6 text-2xl font-medium"
             />
           </div>
         </div>
-        <button className="w-[193px] h-[53px] px-[63px] py-2 rounded-[20px] bg-white flex justify-center items-center text-[24px] font-bold self-center">
+
+        <button className="w-full md:w-[193px] h-[53px] px-[63px] py-2 rounded-[20px] bg-white flex justify-center items-center text-[24px] font-bold self-center">
           Kirim
         </button>
       </div>
